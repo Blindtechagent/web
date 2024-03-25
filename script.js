a837bcc1625ffcbdc11e22cf709610c7a5210a51
Blind tech agent <pawanpianoartist@gmail.com>
2024-03-25 08:33:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -66,3 +66,69 @@ function getGreeting(name) {
     return `Good night ${name}! It's time to sleep. We are so glad to see you at late night.`;
   }
 }
+
+$(document).ready(function () {
+  const accountBtn = document.getElementById("accountBtn");
+  const accountBox = document.querySelector('#accountBox');
+  const loginBtn = document.getElementById("loginBtn");
+  const menuBtn = document.querySelector(".menuBtn");
+  const menuItems = document.getElementById("menuItems");
+
+  if (accountBtn && accountBox && loginBtn) {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        accountBtn.innerHTML = "Manage Account";
+        accountBtn.href = "./menu/manageAccount.html";
+        accountBox.style.display = 'none';
+        loginBtn.style.display = 'none';
+      } else {
+        accountBtn.innerHTML = "Create Account";
+        accountBtn.href = "./menu/createAccount.html";
+        accountBox.style.display = 'block';
+        loginBtn.style.display = 'block';
+        loginBtn.href = "./menu/login.html";
+      }
+    });
+
+    // Define the toggleDrawer function
+    function toggleDrawer() {
+      const isHidden = menuItems.style.display === "none" || menuItems.style.display === "";
+      menuItems.style.display = isHidden ? "block" : "none";
+      menuBtn.setAttribute("aria-expanded", !isHidden);
+      menuBtn.setAttribute("aria-label", isHidden ? "Close Navigation Menu" : "Open Navigation Menu");
+    }
+
+    // Attach the toggleDrawer function to the menu button click event
+    menuBtn.addEventListener("click", toggleDrawer);
+  }
+});
+
+    fetch('https://type.fit/api/quotes')
+      .then(response => response.json())
+      .then(data => {
+        const index = Math.floor(Math.random() * data.length);
+        const thought = data[index].text;
+        document.getElementById('thought').innerHTML = thought;
+      })
+      .catch(err => console.log(err));
+
+    function translateToHindi() {
+      const thought = document.getElementById('thought').innerHTML;
+      fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=hi&dt=t&q=${thought}`)
+        .then(response => response.json())
+        .then(data => {
+          const translated = data[0][0][0];
+          document.getElementById('thought').innerHTML = translated;
+          announce("translated successfully");
+        })
+        .catch(err => console.log(err));
+    }
+  function announce(message) {
+  var announcement = document.getElementById("announcement");
+  announcement.textContent = message;
+  setTimeout(function() {
+    announcement.style.display = "none";
+  }, 3000);
+  announcement.style.display = "block";
+}
+
