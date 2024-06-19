9484eeeb04f43a3b67cf1d481b221b0ecc37f49a
Blind tech agent <pawanpianoartist@gmail.com>
2024-06-19 11:24:25 +0530

    Add files via upload

--- features/ttp.js
+++ features/ttp.js
@@ -1,27 +1,56 @@
+var content = document.getElementById('txtContent'),
+    fileNameInput = document.getElementById('fileName'),
+    buttonDownload = document.getElementById('btnDownload'),
+    buttonGenerate = document.getElementById('btnGenerate'),
+    downloadBox = document.getElementById('downloadBox'),
+    form = document.getElementById('form'),
+    pdfSizeElement = document.getElementById('pdf-size');
 
-      var content = document.getElementById('txtContent'),
-        fileNameInput = document.getElementById('fileName'),
-        buttonDownload = document.getElementById('btnDownload'),
-        buttonGenerate = document.getElementById('btnGenerate'),
-        downloadBox = document.getElementById('downloadBox');
+let pdfDocGenerator = null;
 
-      function downloadPDF() {
-        var fileName = fileNameInput.value || 'my.pdf';
-        var doc = new jsPDF();
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    announce("Generating PDF");
+    setTimeout(() => {
+        generatePDF();
+    }, 1000);
+});
 
-        doc.setFontSize(14);
+function generatePDF() {
+    const docDefinition = {
+        content: [
+            { text: content.value }
+        ]
+    };
 
-        var contentLines = doc.splitTextToSize(content.value, 170);
-        doc.text(20, 20, contentLines);
-        doc.save(fileName);
-      }
-
-      function generatePDF() {
+    pdfDocGenerator = pdfMake.createPdf(docDefinition);
+    pdfDocGenerator.getBlob((blob) => {
+        const size = (blob.size / 1024).toFixed(2);
+        const sizeText = size >= 1000 ? (size / 1000).toFixed(2) + ' MB' : size + ' KB';
+        pdfSizeElement.textContent = 'Size: ' + sizeText;
+        announce('PDF Generated successfully. Please enter a file name and click on Download PDF button.');
         downloadBox.style.display = "block";
-        buttonGenerate.disabled = true;
-        announce("PDF generated successfully");
-      }
+        form.style.display = "none";
+        content.value = '';
+        fileNameInput.placeholder = 'enter the file name here';
+    });
+}
 
-      buttonGenerate.addEventListener('click', generatePDF);
-      buttonDownload.addEventListener('click', downloadPDF);
-    
+function downloadPDF() {
+    const pdfName = fileNameInput.value;
+    if (pdfName) {
+        announce('Downloading PDF...');
+        setTimeout(() => {
+            pdfDocGenerator.download(pdfName + '.pdf', () => {
+                announce('Download Completed!');
+                downloadBox.style.display = "none";
+                form.style.display = "block";
+                content.value = '';
+            });
+        }, 1500);
+    } else {
+        announce('Please enter a file name.');
+    }
+}
+
+buttonDownload.addEventListener('click', downloadPDF);
