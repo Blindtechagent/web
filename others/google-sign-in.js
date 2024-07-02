1ae9499f95bddcb8ef01fdaed8df4e1b02519853
Blind tech agent <pawanpianoartist@gmail.com>
2024-07-02 20:34:52 +0530

    Add files via upload

--- others/google-sign-in.js
+++ others/google-sign-in.js
@@ -1,19 +1,10 @@
-document.getElementById('google-sign-in').addEventListener('click', () => {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    
-    firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-            announce('successfully sign in');
-            window.location.href = '../index.html';
-        })
-        .catch((error) => {
-            console.error(error);
-            if (error.code === 'auth/network-request-failed') {
-                announce('Network error, please check your internet connection and try again');
-            } else if (error.code === 'auth/invalid-email') {
-                announce('Invalid email, please enter a valid email address');
-            } else {
-                announce('Error during Google sign in, please try again later');
-            }
-        });
+document.getElementById('google-sign-in').addEventListener('click', function() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+      window.location.href = '../index.html';
+    })
+    .catch(function(error) {
+      console.error(error.message);
+    })
 });
