e05809a6936d6705c889fbc50b5e32c48c7b1bce
BlindTechAgent <pawanpianoartist@gmail.com>
2024-11-08 09:09:04 +0530

    changes

--- menu/joinUs.js
+++ menu/joinUs.js
@@ -5,11 +5,12 @@ function sendMsg(event) {
     
     var name = document.querySelector('#name').value;
     var email = document.querySelector('#email').value;
+    var whatsappNumber = document.querySelector('#wa').value;
     var userMessage = document.querySelector('#message').value;
     var role = document.querySelector('#role');
     var selectedRoles = Array.from(role.selectedOptions).map(option => option.text).join(', ');
 
-    var sendMessage = 'New Message' + '%0A' + 'Name: ' + name + '%0A' + 'Email: ' + email + '%0A' + 'role: ' + selectedRoles + '%0A' + 'other details: ' + userMessage;
+    var sendMessage = 'New Message' + '%0A' + 'Name: ' + name + '%0A' + 'Email: ' + email + '%0A' + 'WhatsApp number: ' + whatsappNumber + '%0a' + 'role: ' + selectedRoles + '%0A' + 'other details: ' + userMessage;
 
     var telegramChatId = '1677053343';
     var BotToken = 'NjgzNzE1NTc0ODpBQUVRSEJYZGdYR1paODgwLUt5NHo4VFUxU1M1TlR2WF8zTQ==';
@@ -23,7 +24,7 @@ function sendMsg(event) {
         if (xhr.status >= 200 && xhr.status < 300) {
             var responseData = JSON.parse(xhr.responseText);
             console.log(responseData);
-            alert('form submitted Successfully');
+            alert('form submitted Successfully, keep checking your email and whatsapp time to time as we will contact you soon.');
             document.getElementById("successMessage").style.display = "block";
             document.getElementById("joinForm").reset();
         } else {
